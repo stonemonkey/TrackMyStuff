afe1ad55ba1dcbb21f170afe5a3c964d843843b9
Costin Morariu <costin.morariu@svea.com>
2019-12-19 16:05:16 +0100

    Adds Seq for logging visualization and filtering

--- src/TrackMyStuff.ApiGateway/Program.cs
+++ src/TrackMyStuff.ApiGateway/Program.cs
@@ -53,14 +53,14 @@ namespace TrackMyStuff.ApiGateway
 
         private static Serilog.ILogger CreateSerilogLogger(IConfiguration configuration)
         {
-            //var seqServerUrl = configuration["Serilog:SeqServerUrl"];
+            var seqServerUrl = configuration["Serilog:SeqServerUrl"];
             //var logstashUrl = configuration["Serilog:LogstashgUrl"];
             return new LoggerConfiguration()
                 .MinimumLevel.Verbose()
                 .Enrich.WithProperty("ApplicationContext", AppName)
                 .Enrich.FromLogContext()
                 .WriteTo.Console()
-                //.WriteTo.Seq(string.IsNullOrWhiteSpace(seqServerUrl) ? "http://seq" : seqServerUrl)
+                .WriteTo.Seq(string.IsNullOrWhiteSpace(seqServerUrl) ? "http://seq" : seqServerUrl)
                 //.WriteTo.Http(string.IsNullOrWhiteSpace(logstashUrl) ? "http://logstash:8080" : logstashUrl)
                 .ReadFrom.Configuration(configuration)
                 .CreateLogger();
